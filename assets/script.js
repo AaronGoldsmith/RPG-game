f7d7f2237ff9dd895b5ca20af557817d8a36df3d
Aaron Goldsmith <agoldsmith@scu.edu>
2018-08-04 19:17:09 -0700

    pushing a working version. fixed annoying bug that wouldnt display one stat

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,55 @@
+let playerObj = {
+    stats: {
+          health: 10,
+          strength: 1,
+          logic: 1,
+          agility: 1,
+        },
+    items: [],
+};
 
-$(".itemFrame").on("click","i",function(){
-    console.log($(this)["0"])
-    if($(this).hasClass("activate")){
-        $(this).removeClass("activate");
+function updateDisplayedStats(){
+    $("#health").text(playerObj.stats.health)
+    $("#strength").text(playerObj.stats.strength)
+    $("#logic").text(playerObj.stats.logic)
+    $("#agility").text(playerObj.stats.agility)
+}
+$(document).ready(function(){
+    updateDisplayedStats()
+});
+
+
+$(".itemFrame").on("click",function(event){
+    updateDisplayedStats()
+
+    var icon = $(this).find("i");
+    var textName = $(this).find(".itemLabel");
+    var idName = $(this).attr("data-type"); // heartbeat bolt .. etc
+
+    console.log("You chose " + idName.toUpperCase());
+    playerObj.stats[idName]
+    $(this).prepend("<span class='itemVal' id="+idName+"></span>");
+
+    if(icon.hasClass("activate")){
+        // moves the item to bottom of its container
+        // probably a better way to add the score
+        $(this).addClass("align-self-end");
+        updateDisplayedStats()
+
+        textName.removeClass("text-white")
+        icon.removeClass("activate");
     }
     else{
-        
-        $(this).addClass("activate");
+        // moves item to the center
+        var textVal = $(this).find(".itemVal");
+        $(this).removeClass("align-self-end")
+        textName.addClass("text-white")
+        textVal.addClass("invisible")
+        textVal.remove();
+        icon.addClass("activate");
     }
+    updateDisplayedStats()
+
 });
      
           
