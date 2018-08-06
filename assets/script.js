a9a56ea0a87d22df7e4801b52226bc1dca0e9b7b
Aaron Goldsmith <agoldsmith@scu.edu>
2018-08-05 21:33:05 -0700

    begin js logic of gameplay

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 let weaponList = [{"sword":4},{"axe":3},{"pistol":8},{"magic": 7}];
 var skillLabel =  $("#showSkills")
 var skills = $("#skillChart")
-
+var insBtn = $("instructions")
 let playerObj = {
     stats: {
           health: 0,
@@ -15,26 +15,31 @@ let playerObj = {
     }
 };
 function badGuy(named, level){
-    var enemy1 = {
-        name: named,
-        stats:{
-            health: 0,
-            strength: 0,
-            money: 0
-        },
-        difficulty: level,
-        weapons: [weaponList[0]],
+    // var enemy1 = {
+    //     name: named,
+    //     stats:{
+    //         health: 0,
+    //         strength: 0,
+    //         money: 0
+    //     },
+    //     difficulty: level,
+    //     weapons: [weaponList[0]],
         
-        takeDamage(damage){
-            health -= damage;
-            strength -= 1;
-        }
-    };
+    //     takeDamage(damage){
+    //         health -= damage;
+    //         strength -= 1;
+    //     }
+    // };
     this.name = named;
-
-    this.strength = Math.pow(level,2);
+    this.strength = Math.pow(level,2); 
+    this.warcry = function(){
+        return "RAHHH I AM " + this.name;
+    }
 
 }
+
+let level1 = [new badGuy("Maglah",1),new badGuy("Taglah",1),new badGuy("Raglah",1),new badGuy("Naglah",1),new badGuy("Waglah",1)]
+let level2 = [new badGuy("Koba",2),new badGuy("Roba",2),new badGuy("Doba",2),new badGuy("Koba",2),new badGuy("Loba",2),]
 
 // update stats on scren
 function updateDisplayedStats(){
@@ -46,51 +51,50 @@ function updateDisplayedStats(){
 
 $(document).ready(function(){
     updateDisplayedStats()
-
-});
-
-skillLabel.on("click",function(){
-    skills.addClass("visible");
-    skills.removeClass("invisible");
-    skillLabel.removeClass("visible");
-    skillLabel.addClass("invisible")
-})
-$("#hideme").on("click",function(){
-    skills.addClass("invisible");
-    skillLabel.removeClass("invisible");
-    skillLabel.addClass("visible");
-});
-// clicking on a skill
-$(".itemFrame").on("click",function(event){
-    updateDisplayedStats()
-
-    var icon = $(this).find("i");
-    var textName = $(this).find(".itemLabel");
-    var idName = $(this).attr("data-type");
-
-    console.log("You chose " + idName.toUpperCase());
-    playerObj.stats[idName]
-    $(this).prepend("<span class='itemVal rounded-bottom' id="+idName+"></span>");
-
-    if(icon.hasClass("activate")){
-        // moves the item to bottom of its container
-        // probably a better way to add the score
-        $(this).addClass("align-self-end");
+    skillLabel.on("click",function(){
         updateDisplayedStats()
-
-        textName.removeClass("text-white")
-        icon.removeClass("activate");
-        $("#skillInstruct").attr("class","d-none")
-
-    }
-    else{
-        var textVal = $(this).find(".itemVal");
-        $(this).removeClass("align-self-end")        
-        textVal.remove();
-        icon.addClass("activate");
-    }
-    updateDisplayedStats()
+        skills.fadeIn("fast",function(){
+            skills.css("visibility","visible")
+            skillLabel.addClass("invisible")
+        });
+    })
+    $("#hideme").on("click",function(){
+        skills.fadeOut("fast",function(){
+              skills.css("visibility","hidden");
+              skillLabel.removeClass("invisible");
+        });
+    });
+    // clicking on a skill
+    $(".itemFrame").on("click",function(event){
+        updateDisplayedStats()
+        var icon = $(this).find("i");
+        var textName = $(this).find(".itemLabel");
+        var idName = $(this).attr("data-type");
+    
+        console.log("You chose " + idName.toUpperCase());
+        playerObj.stats[idName]
+        $(this).prepend("<span class='itemVal rounded-bottom' id="+idName+"></span>");
+    
+        if(icon.hasClass("activate")){
+            $(this).addClass("align-self-end");
+            // updateDisplayedStats()
+    
+            textName.removeClass("text-white")
+            icon.removeClass("activate");
+            $("#skillInstruct").attr("class","d-none")
+    
+        }
+        else{
+            var textVal = $(this).find(".itemVal");
+            $(this).removeClass("align-self-end"); // reposition item     
+            textVal.remove(); // remove text so element can shift appropriately
+            icon.addClass("activate");
+        }
+        updateDisplayedStats()
+    });
 
 });
+
+
      
           
